cd1f7ebbce2554ac92e7c8d7525fda5442410a76
fabricedupre <32126510+duprefm@users.noreply.github.com>
2019-06-21 16:58:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,51 +17,9 @@ REGION = '${var.aws-region-name}'
 # If it does not exist, it creates the file.
 
 if not os.path.exists(KUBE_FILEPATH):
-    
-    kube_content = dict()
-    # Get data from EKS API
-    eks_api = boto3.client('eks',region_name=REGION)
-    cluster_info = eks_api.describe_cluster(name=CLUSTER_NAME)
-    certificate = cluster_info['cluster']['certificateAuthority']['data']
-    endpoint = cluster_info['cluster']['endpoint']
 
-    # Generating kubeconfig
-    kube_content = dict()
-    
-    kube_content['apiVersion'] = 'v1'
-    kube_content['clusters'] = [
-        {
-        'cluster':
-            {
-            'server': endpoint,
-            'certificate-authority-data': certificate
-            },
-        'name':'kubernetes'
-                
-        }]
-
-    kube_content['contexts'] = [
-        {
-        'context':
-            {
-            'cluster':'kubernetes',
-            'user':'lambda'
-            },
-        'name':'aws'
-        }]
-
-    kube_content['current-context'] = 'aws'
-    kube_content['Kind'] = 'config'
-    kube_content['users'] = [
-    {
-    'name':'aws',
-    'user':'lambda'
-    }]
-
-    print(kube_content)
-    # Write kubeconfig
-    with open(KUBE_FILEPATH, 'w') as outfile:
-        yaml.dump(kube_content, outfile, default_flow_style=False)
+    os.rename("/var/task/kubeconfig", "/tmp/kubeconfig")
+    os.rename("/var/task/aws-iam-authenticator", "/tmp/aws-iam-authenticator")
 
 def handler(event, context):
 
@@ -76,7 +34,7 @@ def handler(event, context):
     # API
     api = client.ApiClient(configuration)
     v1 = client.CoreV1Api(api)
-    
+
     print(open(KUBE_FILEPATH).read())
     # Get all the pods
     ret = v1.list_namespaced_pod("default")
